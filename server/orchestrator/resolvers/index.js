ad373afb45c367ec41e217d8556c6a1b914b34a6
dhimasadip <dhimasadip.dap@gmail.com>
2020-07-18 15:51:31 +0700

    add delete mutation

--- server/orchestrator/resolvers/index.js
+++ server/orchestrator/resolvers/index.js
@@ -42,6 +42,39 @@ const resolvers = {
         .catch(console.log)
     }
   },
+  Mutation: {
+    addMovie: (_, args) => {
+      const { title, overview, poster_path, popularity, tags } = args.movie
+
+      return movieServer.post('/', { title, overview, poster_path, popularity, tags })
+        .then(({ data }) => data.ops[0])
+        .catch(console.log)
+    },
+
+    addTv: (_, args) => {
+      const { title, overview, poster_path, popularity, tags } = args.tv
+
+      return tvServer.post('/', { title, overview, poster_path, popularity, tags })
+        .then(({ data }) => data.ops[0])
+        .catch(console.log)
+    },
+
+    deleteMovie: (_, args) => {
+      const { _id } = args
+
+      return movieServer.delete(`/${_id}`)
+        .then(({ data }) => data)
+        .catch(console.log)
+    },
+
+    deleteTv: (_, args) => {
+      const { _id } = args
+
+      return tvServer.delete(`/${_id}`)
+        .then(({ data }) => data)
+        .catch(console.log)
+    }
+  }
 }
 
 module.exports = resolvers
--- server/orchestrator/schema/index.js
+++ server/orchestrator/schema/index.js
@@ -25,12 +25,41 @@ const typeDefs = gql`
     tvs: [Tv]
   }
 
+  type deleteResult {
+    n: String
+    ok: String
+    deletedCount: String
+  }
+
   type Query {
     movies: [Movie]
     tvs: [Tv]
     movie(_id: String): Movie
     tv(_id: String): Tv
     entertainme: AllItems
+  }
+
+  input MovieInput {
+    title: String
+    overview: String
+    poster_path: String
+    popularity: Float
+    tags: String
+  }
+
+  input TvInput {
+    title: String
+    overview: String
+    poster_path: String
+    popularity: Float
+    tags: String
+  }
+
+  type Mutation {
+    addMovie(movie: MovieInput ) : Movie
+    addTv(tv: TvInput ) : Tv
+    deleteMovie(_id: String) : deleteResult
+    deleteTv(_id: String) : deleteResult
   }
 `
 
